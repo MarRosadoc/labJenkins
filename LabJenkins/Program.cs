41a2d8cdb404ae66afb89515de5f52976fe13232
Mar Rosado <67706376+MarRosadoc@users.noreply.github.com>
2024-03-15 18:30:05 -0600

    Add files via upload

---
+++ LabJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
